c58f1ae4720e2ffe4173458c17444ef8f7853401
Masato Miyamoto <project-europa@excite.co.jp>
2020-05-14 10:03:11 +0900

    mod: レイアウトの設定を追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
-import router from './router'
-import App from './layouts/default'
-import ctx from './plugin/vuetify'
+import router from '~/router'
+import App from '~/App.vue'
+import vuetify from '~/plugin/vuetify'
+import AuthPlugin from '~/plugin/auth'
+import DefaultLayout from '~/layouts/default';
+import NoAuthLayout from '~/layouts/noauth';
 
-Vue.use(ctx)
+Vue.use(AuthPlugin)
+
+Vue.component('default-layout', DefaultLayout);
+Vue.component('noauth-layout', NoAuthLayout);
 
 new Vue({
+  vuetify,
   router: router,
   render: h => h(App),
 }).$mount('#app')
