03eb3ae84793c7f9d29d3f9da1d15361b40620c6
Masato Miyamoto <project-europa@excite.co.jp>
2020-09-08 14:12:57 +0900

    fix: typo

--- app/Http/Controllers/SyncGoogleController.php
+++ app/Http/Controllers/SyncGoogleController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Spatie\GoogleCalendar\Event;
+use Auth;
 
-class SyncGoogleContoller extends Controller
+class SyncGoogleController extends Controller
 {
     //
     public function sync(Request $request)
@@ -16,5 +17,17 @@ class SyncGoogleContoller extends Controller
         $event->startDateTime = new Carbon("{$request->start_date} {$request->start_date_time}");
         $event->endDateTime = new Carbon("{$request->end_date} {$request->end_date_time}");
         $event->save();
+    }
+
+    /**
+     *
+     */
+    public function isAuth() {
+        try {
+            Event::get(null, null, [], Auth::user()->email);
+        } catch (\Exception $e) {
+            return ['google_auth' => 0];
+        }
+        return ['google_auth' => 1];
     }
 }
